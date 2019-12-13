4d5a93cbed1b0cbc71352206c8601c3ff1674488
Yoshiaki Miyazaki <ymiyazakixyz@gmail.com>
2019-12-13 18:08:54 +0900

    fixed cmd. remove binary.

---
+++ cmd/gcp/files/main.template.tf
@@ -0,0 +1,36 @@
+#--------------------------------------------------------------
+# main_init.tf must be not touch! because main_init.tf is auto generate file.
+# If you want to fix it, you should be fix /shell/files/main.template.tf.
+#--------------------------------------------------------------
+
+#--------------------------------------------------------------
+# terraform state
+#--------------------------------------------------------------
+terraform {
+  required_version = ">= 0.12"
+  backend "gcs" {
+    credentials = "##GOOGLE_CLOUD_KEYFILE_JSON##"
+    bucket      = "##BUCKET##"
+    prefix      = "##PREFIX##"
+  }
+}
+
+#--------------------------------------------------------------
+# Provider Setting
+#--------------------------------------------------------------
+provider "google" {
+  credentials = "${file("##GOOGLE_CLOUD_KEYFILE_JSON##")}"
+  project     = "##PROJECT_ID##"
+  region      = "##REGION##"
+  zone        = "##ZONE##"
+}
+provider "google-beta" {
+  credentials = "${file("##GOOGLE_CLOUD_KEYFILE_JSON##")}"
+  project     = "##PROJECT_ID##"
+  region      = "##REGION##"
+  zone        = "##ZONE##"
+}
+#--------------------------------------------------------------
+# Information
+#--------------------------------------------------------------
+data "google_project" "project" {}
