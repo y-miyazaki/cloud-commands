005ab299f6dbc5d49842b35e0ef17c0caacf53b6
Yoshiaki Miyazaki <ymiyazakixyz@gmail.com>
2020-06-03 15:53:08 +0900

    fixed install.sh. fixed settings.json.

--- install.sh
+++ install.sh
@@ -5,16 +5,24 @@
 set -e
 
 # variables
+CLOUD_COMMANDS_VERSION=$1
 INSTALL_DIR=/usr/local/bin
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
+# download cmd.zip and unzip
+curl -L -o cmd.zip https://github.com/y-miyazaki/cloud-commands/releases/download/${CLOUD_COMMANDS_VERSION}/cmd.zip
+unzip -d $SCRIPT_DIR/cloud-commands cmd.zip
+
 # create directory and copy commands
-# mkdir -p $INSTALL_DIR
 # chmod commands
-find $SCRIPT_DIR/cmd -maxdepth 1 -type f | xargs chmod 775
+find $SCRIPT_DIR/cloud-commands/cmd -maxdepth 1 -type f | xargs chmod 775
 
 # chmod files
-chmod 775 $SCRIPT_DIR/cmd/files
-find $SCRIPT_DIR/cmd/files -type f | xargs chmod 644
+chmod 775 $SCRIPT_DIR/cloud-commands/cmd/files
+find $SCRIPT_DIR/cloud-commands/cmd/files -type f | xargs chmod 644
 # copy commands
-sudo cp -rp ${SCRIPT_DIR}/cmd/* $INSTALL_DIR
+sudo cp -rp $SCRIPT_DIR/cloud-commands/cmd/* $INSTALL_DIR
+
+# remove files
+rm -f cmd.zip
+rm -rf $SCRIPT_DIR/cloud-commands
