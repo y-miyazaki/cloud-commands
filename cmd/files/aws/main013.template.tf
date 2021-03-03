06e0a28e892e35cac537eac2b1216f8199d35f7f
y-miyazaki <ymiyazakixyz@gmail.com>
2021-03-03 13:12:40 +0900

    fixed aws commands.

---
+++ cmd/files/aws/main013.template.tf
@@ -0,0 +1,38 @@
+#--------------------------------------------------------------
+# main_init.tf must be not touch! because main_init.tf is auto generate file.
+# If you want to fix it, you should be fix shell/aws/files/main.template.tf.
+#--------------------------------------------------------------
+
+#--------------------------------------------------------------
+# terraform state
+#--------------------------------------------------------------
+terraform {
+  required_version = "##REQUIRED_VERSION##"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "##AWS_VERSION##"
+    }
+  }
+  backend "s3" {
+    bucket  = "##S3_BUCKET##"
+    key     = "##S3_KEY##"
+    profile = "default"    # fix for environment
+    region  = "##S3_REGION##" # fix for environment
+  }
+}
+
+#--------------------------------------------------------------
+# Provider Setting
+# access key and secret key should not use.
+#--------------------------------------------------------------
+provider "aws" {
+  profile = "default"    # fix for environment
+  region  = "##REGION##" # fix for environment
+}
+
+#--------------------------------------------------------------
+# my account id/region
+#--------------------------------------------------------------
+data "aws_caller_identity" "self" {}
+data "aws_region" "self" {}
